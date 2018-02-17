cfa1a6774246e1259b52aea4b12cef73c3badbeb
Matthew Carter <m@ahungry.com>
2018-02-16 22:45:33 -0500

    Add easy way to launch REPL

--- j.el
+++ j.el
@@ -1,4 +1,19 @@
 ;; Some code for J (APL)
 
+;;; Code:
+
+(defvar j-console-bin "~/jbld/j64/bin/jconsole"
+  "The location of the jconsole binary.
+
+Set to a default GNU/Linux build directory, as jvm also installs
+a jconsole in PATH.")
+
+(defun j-repl ()
+  "Launch a J repl."
+  (interactive)
+  ;; (comint-run j-console-bin)
+  (switch-to-buffer (make-comint "jconsole" j-console-bin))
+  )
+
 (provide 'j)
 ;;; j.el ends here
