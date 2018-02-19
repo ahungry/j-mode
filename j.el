38ceb98e1a4f6606f43c4b62cc5aea562d533f77
Matthew Carter <m@ahungry.com>
2018-02-18 22:59:02 -0500

    Tweak settings

--- j.el
+++ j.el
@@ -10,23 +10,23 @@
 Set to a default GNU/Linux build directory, as jvm also installs
 a jconsole in PATH.")
 
+;;;###autoload
 (defun j-repl ()
   "Launch a J repl."
   (interactive)
   ;; (comint-run j-console-bin)
-  (switch-to-buffer (make-comint "jconsole" j-console-bin))
-  )
+  (switch-to-buffer (make-comint "jconsole" j-console-bin)))
 
 (defconst j-mode-font-lock-keywords-1
   (list
+   '("NB\\..*" . font-lock-comment-face)
    '("\\([[:lower:]]+?\\)=" 1 font-lock-function-name-face)
    '("=[:.]" . font-lock-keyword-face)
-   '("NB\\..*" . font-lock-comment-face)
    '("[[:upper:]][[:lower:]]*" . font-lock-variable-name-face)
    ))
 
 ;;;###autoload
-(define-derived-mode j-mode text-mode "J" ()
+(define-derived-mode j-mode fundamental-mode "J" ()
   "Major mode for editing J files."
   :group 'languages
   (set (make-local-variable 'font-lock-defaults)
