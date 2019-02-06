e5c8dd435f2f3173d54a1944315dcd69d237be8e
Matthew Carter <m@ahungry.com>
2019-02-06 00:23:31 -0500

    Add header

--- j.el
+++ j.el
@@ -1,3 +1,32 @@
+;;; j-mode.el --- Major mode for editing J (APL like)  files.  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018, 2019 Matthew Carter <m@ahungry.com>
+
+;; Author: Matthew Carter <m@ahungry.com>
+;; Maintainer: Matthew Carter <m@ahungry.com>
+;; URL: https://github.com/ahungry/j-mode
+;; Version: 0.0.1
+;; Date: 2019-02-06
+;; Keywords: languages, j,
+;; Package-Requires: ((emacs "25.1") (cl-lib "0.6.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
 ;; Some code for J (APL)
 
 ;;; Code:
